c0f63d1ffe91232340f8ca0f384d841472d22108
Angel Palacios Mirafuentes <al221910938@gmail.com>
2020-10-26 22:35:35 -0600

    Migraciones Angel

---
+++ database/migrations/2020_10_27_041241_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id_producto');
+            $table->integer('clave');
+            $table->string('nombre',30);
+            $table->string('precio',30);
+            $table->string('existencias',30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
